bd8a5a2cb1440e407c1b1ee8b975af44ad87fcab
Pagodaimedia <111962549+Pagodaimedia@users.noreply.github.com>
2023-12-21 17:15:29 +0800

    Create OneSignalSDKWorker.js

---
+++ push/OneSignalSDKWorker.js
@@ -0,0 +1,4 @@
+// OneSignal ServiceWorker
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+// And some other site specific code in the same file example.
+importScripts('https://www.pagodaimedia.my.id/sw.js');
