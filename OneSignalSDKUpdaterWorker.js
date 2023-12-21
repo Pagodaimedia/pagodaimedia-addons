81ab5c13bb3a8e1d715bad42a94948b556ecc0f2
Pagodaimedia <111962549+Pagodaimedia@users.noreply.github.com>
2023-12-21 17:02:24 +0800

    Delete OneSignalSDKUpdaterWorker.js

--- OneSignalSDKUpdaterWorker.js
+++
@@ -1,4 +0,0 @@
-// OneSignal ServiceWorker
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-// And some other site specific code in the same file example.
-importScripts('https://www.pagodaimedia.my.id/sw.js');
